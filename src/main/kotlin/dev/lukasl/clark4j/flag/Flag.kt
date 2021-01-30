13f6210ff224c869975f18d5c91af7494f34b6e7
lukasl-dev <git@lukasl.dev>
2021-01-30 16:06:32 +0100

    Create enum class Flag.kt

---
+++ src/main/kotlin/dev/lukasl/clark4j/flag/Flag.kt
@@ -0,0 +1,22 @@
+package dev.lukasl.clark4j.flag
+
+enum class Flag(val flag: String) {
+  INPUT(
+  flag = "--input"
+  ),
+  PREFIX(
+  flag = "--prefix"
+  ),
+  PREFIX_IGNORE_CASE(
+  flag = "--prefix-ignore-case"
+  ),
+  LABEL(
+  flag = "--label"
+  ),
+  LABEL_IGNORE_CASE(
+  flag = "--label-ignore-case"
+  ),
+  ADVANCED(
+  flag = "--advanced"
+  );
+}
